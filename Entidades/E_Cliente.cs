89d624b645ce9541da94c6df8611d135a5cf2b5e
kopa2 <kopacek5@gmail.com>
2025-05-18 15:09:21 -0300

    Se agrega la clase que funciona como Entidad E_Cliente

---
+++ Entidades/E_Cliente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuPrincipalClub.Entidades
+{
+    internal abstract class E_Cliente
+    {
+        /*
+         En la proxima iteracion se buscara implementar la clase Cuota en el constructor y seimplementara la encapsulazion de los atributos haciendolos accesibles atravez de metodos Set y Get (Por el momentose deja asi porque se busca poder probar de agregar clientes a la DB de manera rápida).
+         */
+
+        public int? idCliente;
+        public string Nombre, Apellido, Telefono, NumeroDocumento;
+        public bool Activo;
+       // public E_Cuota cuota;
+
+
+        public E_Cliente(string nombre, string apellido, string telefono, string numeroDocumento, bool activo)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Telefono = telefono;
+            NumeroDocumento = numeroDocumento;
+            Activo = activo;
+        }
+
+        public bool registrar()
+        {
+            return Activo;
+        }
+
+        public bool pagar()
+        {
+            return false;
+        }
+
+
+    }
+}
