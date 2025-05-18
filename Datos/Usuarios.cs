c8d1c21d55d35663e7a24894078fc35477346518
kopa2 <kopacek5@gmail.com>
2025-05-18 12:16:15 -0300

    Se agrega la clase Usuario, para gestionar ingreso de los administradores del sistema

---
+++ Datos/Usuarios.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace MenuPrincipalClub.Datos
+{
+    internal class Usuarios
+    {
+        public DataTable LogInUsuario(string usuario, string contrasenia)
+        {
+            MySqlDataReader resultado;
+            DataTable tabla = new DataTable();
+            MySqlConnection sqlCon = new MySqlConnection();
+
+            try
+            {
+                sqlCon = Conexion.getInstancia().CrearConexion();
+                MySqlCommand comando = new MySqlCommand("IngresoLogin", sqlCon);
+
+                comando.CommandType = CommandType.StoredProcedure;
+
+                comando.Parameters.Add("Usu", MySqlDbType.VarChar).Value = usuario;
+                comando.Parameters.Add("Pass", MySqlDbType.VarChar).Value = contrasenia;
+
+                sqlCon.Open();
+                resultado = comando.ExecuteReader();
+                tabla.Load(resultado);
+
+                return tabla;
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+        }
+    }
+}
