0fe6efa551ade1efd76845febe2d1ba27bbd1ab0
kopa2 <kopacek5@gmail.com>
2025-05-18 12:54:33 -0300

    Se agrega la clase Conexion para gestionar todas las conexiones a la base de datos

---
+++ Datos/Conexion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace MenuPrincipalClub.Datos
+{
+    internal class Conexion
+    {
+        private String nombreDB, svDB, puertoDB, usuario, contrasenia;
+        private static Conexion? con = null;
+
+        private Conexion()
+        {
+            this.nombreDB = "clubdeportivo_lossuperamigos";
+            this.svDB = "127.0.0.1";
+            this.puertoDB = "3306";
+            this.usuario = "root";
+            this.contrasenia = "";
+        }
+
+        public MySqlConnection CrearConexion()
+        {
+            MySqlConnection? cadena = new MySqlConnection();
+
+            try
+            {
+                cadena.ConnectionString = "datasource=" + this.svDB +
+                                          ";port=" + this.puertoDB +
+                                          ";username=" + this.usuario +
+                                          ";password=" + this.contrasenia +
+                                          ";Database=" + this.nombreDB;
+            }
+            catch (Exception ex)
+            {
+                cadena = null;
+                throw;
+            }
+            return cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if (con == null)
+            {
+                con = new Conexion();
+            }
+            return con;
+        }
+
+    }
+}
+
