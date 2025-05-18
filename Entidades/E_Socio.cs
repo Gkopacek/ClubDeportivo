b2187bb1cb1ddc1cb355a7b060a7f3096169036d
kopa2 <kopacek5@gmail.com>
2025-05-18 15:18:38 -0300

    Se agrega la clase hija de E_Cliente que funciona como Entidad la cual es E_Socio

---
+++ Entidades/E_Socio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuPrincipalClub.Entidades
+{
+    internal class E_Socio : E_Cliente
+    {
+        public int? NumeroDeSocio { get; set; }
+        public bool TieneCarnet { get; set; }
+
+        // Constructor de la subclase Socio
+        public E_Socio(string nombre, string apellido, string telefono, string numeroDocumento, bool activo, bool tieneCarnet)
+            : base(nombre, apellido, telefono, numeroDocumento, activo) // Llamada al constructor de Cliente
+        {
+            TieneCarnet = tieneCarnet;
+        }
+
+        // Método ToString para imprimir la información del socio
+        public override string ToString()
+        {
+            return base.ToString() + $", Número de Socio: {NumeroDeSocio}, Tiene Carnet: {TieneCarnet}";
+        }
+    }
+}
