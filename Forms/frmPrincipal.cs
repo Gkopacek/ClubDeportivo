e2902b2786931c6c8784eaf828a51785346c3b88
Carlos Sebastian Gauto <23csebastian@gmail.com>
2025-06-14 19:39:53 -0300

    Refactorizados nombres por convencion del formulario menu principal

--- Forms/frmPrincipal.cs
+++ Forms/frmPrincipal.cs
@@ -30,17 +30,17 @@ namespace MenuPrincipalClub
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
+            //frmBusquedaDNI frmBusquedaDNI = new frmBusquedaDNI();
+            //frmBusquedaDNI.Show();
 
+            //funcion en manetenimiento
+            MessageBox.Show("Función aún no disponible.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
 
-        }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void btnMostrarAfiliados_Click(object sender, EventArgs e)
         {
             // Conexion conexion = Conexion.getInstancia();
             // Conecto conecto = new Conecto("Server=localhost;Database=club_deportivo;User ID=root;Password=root;SslMode=Required;AllowPublicKeyRetrieval=False");
@@ -53,38 +53,25 @@ namespace MenuPrincipalClub
             if (usuarios != null)
             {
                 // Asignar la lista de usuarios al DataGridView
-                dataGridView1.DataSource = usuarios;
-                dataGridView1.Columns["Apto_Fisico"].Visible = false; // oculta el bool
-                dataGridView1.Columns["Apto_Fisico_Descripcion"].HeaderText = "Apto Físico";
+                dbgrdAfiliadosDeudores.DataSource = usuarios;
+                dbgrdAfiliadosDeudores.Columns["Apto_Fisico"].Visible = false; // oculta el bool
+                dbgrdAfiliadosDeudores.Columns["Apto_Fisico_Descripcion"].HeaderText = "Apto Físico";
             }
             else
             {
                 // Manejar el caso en que no se encontraron usuarios
                 MessageBox.Show("No se encontraron usuarios.");
             }
+
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void btnRegistrarPersona_Click(object sender, EventArgs e)
         {
             frmRegistrarSocio frmregistrarsocio = new frmRegistrarSocio();
             frmregistrarsocio.Show();
         }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            //frmBusquedaDNI frmBusquedaDNI = new frmBusquedaDNI();
-            //frmBusquedaDNI.Show();
-
-            //funcion en manetenimiento
-            MessageBox.Show("Función aún no disponible.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-
-        private void button3_Click_1(object sender, EventArgs e)
+        private void btnListaDeudores_Click_1(object sender, EventArgs e)
         {
             // llamos al service y actualizamos lista de deudores en la db
             ServicioCliente servicioCliente = new ServicioCliente();
@@ -95,11 +82,12 @@ namespace MenuPrincipalClub
             //traemos los socios a una lista
             List<Socio> usuarios = servicioCliente.ObtenerSocios();
             // y mostamos en el datagridview los que tienen estado inactivo 
-            if (usuarios != null) {
+            if (usuarios != null)
+            {
                 // Filtramos los usuarios que están inactivos
                 var deudores = usuarios.Where(u => u.Estado == Estado.Inactivo).ToList();
                 // Asignamos la lista filtrada al DataGridView
-                dataGridView1.DataSource = deudores;
+                dbgrdAfiliadosDeudores.DataSource = deudores;
             }
             else
             {
@@ -113,10 +101,10 @@ namespace MenuPrincipalClub
             //MessageBox.Show("Función aún no disponible. 2", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        private void btnRealizarPago_Click(object sender, EventArgs e)
         {
             //abrimos el form para buscar por dni
-            
+
             //verificamos si existe el dni en la db
 
             ServicioCliente servicioCliente = new ServicioCliente();
@@ -133,9 +121,15 @@ namespace MenuPrincipalClub
                 //si no existe mostramos un mensaje de error
                 MessageBox.Show("El usuario con el DNI ingresado no existe.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
 
+        private void dbgrdAfiliadosDeudores_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
+        }
 
+        private void lblMenuOpciones_Click(object sender, EventArgs e)
+        {
 
         }
     }
